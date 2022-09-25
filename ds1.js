d4f11901b78d21183dccfce0725a7438f7256f63
AinurAbdullaeva <114354384+AinurAbdullaeva@users.noreply.github.com>
2022-09-25 22:26:36 +0500

    Add files via upload

---
+++ ds1.js
@@ -0,0 +1,26 @@
+let fs = require ("fs");
+let str = fs.readFileSync("test.txt");
+str = str.toString()
+
+
+let alph = new Array();
+for (let i = 0; i < str.length; i++){
+	alph[str.charAt(i)] = 0;
+}
+for (let i = 0; i < str.length; i++){
+	alph[str.charAt(i)]++;
+}
+let n = 0;
+for (i in alph){
+	alph[i] /= str.length;
+	n++;
+}
+h=0
+for (i in alph){
+	h -= alph[i]*Math.log(alph[i])/Math.log(n);
+	
+}
+console.log(h);
+	
+	
+	
