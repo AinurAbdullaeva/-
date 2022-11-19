99be4c6c6b9f982d435c5bcf2e33cacd15c18589
AinurAbdullaeva <114354384+AinurAbdullaeva@users.noreply.github.com>
2022-11-19 13:30:49 +0500

    Update BruteForce.js

--- BruteForce.js
+++ BruteForce.js
@@ -1,19 +1,23 @@
 let fs = require ("fs");
-let S= fs.readFileSync("str.txt");
+let S= fs.readFileSync("1000000a.txt");
 S = S.toString();
-let T = fs.readFileSync("understr.txt");
+let T = fs.readFileSync("ab.txt");
 T = T.toString();
 let i=0;
 n = S.length;
 m = T.length;
+arr=new Array();
+const start = new Date().getTime();
 while (i<n-m+1){
 	j=0;
 	while (S[i+j]==T[j]){
 		j++;
 		if (j==m){
-			console.log(i);
+			arr.push(i);
 		}
 	}
 	i++;
 }
-
+//cosnole.log(arr);
+const end = new Date().getTime();
+console.log(arr.length, end-start);
