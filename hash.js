0f15067fd32817a76cbf96ee57a51e1aa3ce9989
AinurAbdullaeva <114354384+AinurAbdullaeva@users.noreply.github.com>
2022-11-19 13:31:54 +0500

    Update hash.js

--- hash.js
+++ hash.js
@@ -1,24 +1,42 @@
 let fs = require ("fs");
-let S= fs.readFileSync("str.txt");
+let S= fs.readFileSync("1000000a.txt");
 S = S.toString();
-let T = fs.readFileSync("understr.txt");
+let T = fs.readFileSync("ab.txt");
 T = T.toString();
 let i=0;
 n = S.length;
 m = T.length;
-function hashT(T){
-	let hT=0;
-	for(i=0; i<m; i++){
-		hT+= T.charCodeAt(i);
-		return hT;
+arr=new Array ();
+const start = new Date().getTime();
+function hashT (){
+	let subhash=0;
+	for(let i=0;i<m;i++){
+		subhash+=T.charCodeAt(i)*(2**(m-i-1));
+	}
+	return subhash
+}
+function rehashS(){
+	let hash=0;
+	for(let i=0;i<m;i++){
+		hash+=S.charCodeAt(i)*(2**(m-i-1));
+	}
+	return hash;
+	
+}
+let subhash=hashT();
+let hash=rehashS();
+if  (hash==subhash){
+	arr.push(0);
+}
+for (i=1;i<=n-m;i++){
+	hash=(hash-S.charCodeAt(i-1)*(2**(m-1)))*2+S.charCodeAt(i+m-1);
+	if (hash==subhash){
+		arr.push(i);
 	}
 }
+//console.log(arr);
+const end = new Date().getTime();
+console.log(end-start);
 
-hash=0;
-for(i=1; i<=n-m+1;i++){
-   for(j=i; j<=i+m-1; j++){
-	   hash+=S.charCodeAt(j)*Math.pow(2,n-j);
-   }
-   if (hash== hashT(T)) console.log(i);
-}
+
 
